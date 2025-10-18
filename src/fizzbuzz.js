342211fe99e2462cf73e63ab27df94ba0ca4a095
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-10-18 15:13:18 -0600

    ci(workflow): add name to job in node.js workflow refactor(fizzbuzz): improve code readability and consistency
    Add explicit job name in GitHub workflow and refactor fizzbuzz function with consistent braces and semicolons

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,21 +1,23 @@
 export const fizzbuzz = (number) => {
-  if (typeof number !== 'number')
+  if (typeof number !== 'number') {
     throw new Error('parameter provided must be a number')
+  }
 
-  if (Number.isNaN(number))
+  if (Number.isNaN(number)) {
     throw new Error('parameter provided must be a number')
+  }
 
   const multiplies = {
     3: 'Fizz',
     5: 'Buzz',
-    7: 'Woff',
+    7: 'Woff'
   }
 
   let output = ''
 
   Object.entries(multiplies).forEach(([multiplier, word]) => {
-    if (number % multiplier === 0) output += word;
-  });
+    if (number % multiplier === 0) output += word
+  })
 
   return output === '' ? number : output
 }
